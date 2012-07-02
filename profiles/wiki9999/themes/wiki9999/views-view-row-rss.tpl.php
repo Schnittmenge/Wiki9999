0829f19c9eb433290a5d422104e202fdfcc279ad
Schnittmenge <info@schnittmenge.eu>
2012-07-02 06:22:43 +0200

    template for view rss for entity decode

---
+++ profiles/wiki9999/themes/wiki9999/views-view-row-rss.tpl.php
@@ -0,0 +1,8 @@
+  <item>
+    <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
+    <description>
+    <?php $a = html_entity_decode($description); ?>
+    <?php print $a; ?></description>
+    <?php print $item_elements; ?>
+  </item>
