9ed89de68e5f86c18bb1d0c3f4f7474a2897d420
Schnittmenge <info@schnittmenge.eu>
2012-07-18 16:21:43 +0200

    varios endpoints

--- profiles/wiki9999/themes/wiki9999/views-view-row-rss.tpl.php
+++
@@ -1,7 +0,0 @@
-  <item>
-    <title><?php print $title; ?></title>
-    <link><?php print $link; ?></link>
-
-<description><?php print str_ireplace ("&nbsp;" , " " , htmlspecialchars_decode ($description)); ?></description>
-dsm(@description);
-  </item>
